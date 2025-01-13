0996b33f8b68c3850c47c0ab602e11c3466d4097
Mahdyar M. M. Sadeghi <mir@msadeghi.com>
2025-01-14 01:57:00 +0330

    Fix small bug

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import  BookGrid   from './components/bookgrid';
 
 export default async function Home() {
 
-    const data = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${apiEndpoint}`);
+    const data = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/books`);
     const books = await data.json()
 
     return (
